73c10460e5f76de6e389b8ae854ecace742c9a69
ultrasoft011 <ultrasoft.011@gmail.com>
2021-09-17 16:28:24 -0500

    navbar control

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const myArray = ["A", "B", "C"];
 const first = document.querySelector(".example");
+const profile = document.querySelector(".header--profile");
+const work = document.querySelector(".header--work");
+const about = document.querySelector(".header--about");
+
 console.log(first);
 
 setTimeout(function () {
   first.innerHTML = "I'm Felipe";
-  console.log("Something change with setTimeOut");
   setTimeout(function () {
     first.innerHTML = "A Front-end Developer";
   }, 5000);
+  setTimeout(function () {
+    first.style.visibility = "hidden";
+  }, 10000);
+  setTimeout(function () {
+    profile.style.display = "block";
+  }, 11000);
+  setTimeout(function () {
+    work.style.display = "block";
+  }, 13000);
+  setTimeout(function () {
+    about.style.display = "block";
+  }, 15000);
 }, 5000);
