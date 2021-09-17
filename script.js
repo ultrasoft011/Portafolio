4a9588bbb81a3f27181b9f00d08fd3a9d2c66d30
ultrasoft011 <ultrasoft.011@gmail.com>
2021-09-17 18:51:40 -0500

    Navbar complete

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const first = document.querySelector(".example");
-const profile = document.querySelector(".header--profile");
+const project = document.querySelector(".header--project");
 const work = document.querySelector(".header--work");
 const about = document.querySelector(".header--about");
 
@@ -8,16 +8,16 @@ console.log(first);
 setTimeout(function () {
   first.innerHTML = "I'm Felipe";
   setTimeout(function () {
-    first.innerHTML = "A Front-end Developer";
+    first.innerHTML = "Front-end Developer";
   }, 5000);
   setTimeout(function () {
-    first.style.visibility = "hidden";
+    first.style.display = "none";
   }, 10000);
   setTimeout(function () {
-    profile.style.display = "block";
+    work.style.display = "block";
   }, 11000);
   setTimeout(function () {
-    work.style.display = "block";
+    project.style.display = "block";
   }, 13000);
   setTimeout(function () {
     about.style.display = "block";
